2ddad914ebfbbb4ce62b8ff089ee5004c82c76b9
midCenturyDude7 <mgriffes@gmail.com>
2025-05-31 08:32:25 -0700

    Getting started with JSX syntax vs React.createElement()

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-
-const Pizza = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("p", {}, props.description),
-  ]);
-};
+import Pizza from "./Pizza.jsx";
 
 const App = () => {
   return React.createElement("div", {}, [
