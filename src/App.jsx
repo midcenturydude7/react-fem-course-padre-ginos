e97a7f57ed45d36de6976408420b9ef862d9da2f
midCenturyDude7 <mgriffes@gmail.com>
2025-06-01 21:16:13 -0700

    Minor edit to check for styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Order from "./Order";
 const App = () => {
   return (
     <div>
-      <h1>Padre Gino's - Order Now</h1>
+      <h1 className="logo">Padre Gino's - Order Now</h1>
       <Order />
     </div>
   );
