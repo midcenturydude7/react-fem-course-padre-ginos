8e92d31c874c289cff86527e853177db838723d5
midCenturyDude7 <mgriffes@gmail.com>
2025-06-03 20:08:26 -0700

    Completed the custom hook to display pizza of the day

---
+++ src/hooks/usePizzaOfTheDay.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const usePizzaOfTheDay = () => {
+  const [pizzaOfTheDay, setPizzaOfTheDay] = React.useState(null);
+
+  React.useEffect(() => {
+    async function fetchPizzaOfTheDay() {
+      const response = await fetch("/api/pizza-of-the-day");
+      const data = await response.json();
+      setPizzaOfTheDay(data);
+    }
+
+    fetchPizzaOfTheDay();
+  }, []);
+
+  return pizzaOfTheDay;
+};
